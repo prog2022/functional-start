4f2a4937462b2b43040f4cad574e995806785976
jbrucker <jebrucker@gmail.com>
2022-02-03 08:49:54 +0700

    add starter code

---
+++ appfun.py
@@ -0,0 +1,26 @@
+"""Part 2: Define novel functions as a single statement."""
+from funclib import *
+
+# problem 1
+f = 
+
+# problem 2
+g =
+
+# problem 3
+h =
+
+def doctests():
+    """Examples of using the functions above.
+
+    TODO write doctests for f, g, h
+    
+    """
+    # Don't copy and paste these observed values into your doctests.
+    # Doctests should use known correct results (verify by manual computation).
+    for x in [0, 1, 2]:
+        print(f"f({x}) =", f(x))
+    for x in [0, 1, -3]:
+        print(f"g({x}) =", g(x))
+    for x in [0, 1]:
+        print(f"h({x}) =", h(x))
